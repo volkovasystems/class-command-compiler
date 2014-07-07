77733987718e1f25c7d34c6b07624aa227ec2f47
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-07 16:58:23 +0800

    Added necessary methods for the class CommandCompiler.

--- class-command-compiler.js
+++ class-command-compiler.js
@@ -1,29 +1,96 @@
 /*:
- @module-configuration:
- {
- "packageName": "permutate-lexicographically",
- "fileName": "permutate-lexicographically.js",
- "moduleName": "permutateLexicographically",
- "authorName": "Richeve S. Bebedor",
- "authorEMail": "richeve.bebedor@gmail.com",
- "repository": "git@github.com:volkovasystems/permutate-lexicographically.git",
- "isGlobal": true
- }
- @end-module-configuration
+    @module-configuration:
+        {
+            "packageName": "class-command-compiler",
+            "fileName": "class-command-compiler.js",
+            "moduleName": "CommandCompiler",
+            "className": "CommandCompiler",
+            "authorName": "Richeve S. Bebedor",
+            "authorEMail": "richeve.bebedor@gmail.com",
+            "repository": "git@github.com:volkovasystems/class-command-compiler.git",
+            "isGlobal": true
+        }
+    @end-module-configuration
 
- @module-documentation:
+    @module-documentation:
 
- @end-module-documentation
+    @end-module-documentation
 
- @include:
- @end-include
- */
+    @include:
+        {
+            "arguments-to-array": "argumentsToArray"
+        }
+    @end-include
+*/
 var CommandCompiler = function CommandCompiler( compiler ){
-    this.compiler = compiler;
+    /*:
+        @meta-configuration:
+            {
+                "compiler:required": "Class"
+            }
+        @end-meta-configuration
+    */
+
+    this.setCompiler( compiler );
 };
 
 CommandCompiler.prototype.compile = function compile( ){
-    this.compiler.compile.apply( this, Arra )
+    this.compiler.compile.apply( this, argumentsToArray( arguments ) );
 };
+
+CommandCompiler.prototype.setCompiler = function setCompiler( compiler ){
+    /*:
+        @meta-configuration:
+            {
+                "compiler:required": "Class"
+            }
+        @end-meta-configuration
+    */
+
+    this.compiler = new compiler( );
+};
+
+CommandCompiler.prototype.setCompilerWithNamespace = function setCompilerWithNamespace( namespace ){
+    /*:
+        @meta-configuration:
+            {
+                "namespace:required": "string"
+            }
+        @end-meta-configuration
+    */
+
+    var compiler = this.compilerSet[ namespace ];
+
+    if( "compilerSet" in this &&
+        namespace in this.compilerSet &&
+        "compiler" in this &&
+        this.compiler.constructor !== compiler )
+    {
+        this.setCompiler( compiler );
+    }
+};
+
+CommandCompiler.prototype.setCompilerNamespace = function setCompilerNamespace( namespace, compiler ){
+    /*:
+        @meta-configuration:
+            {
+                "namespace:required": "string",
+                "compiler:required": "Class"
+            }
+        @end-meta-configuration
+    */
+
+    if( !( "compilerSet" in this ) ){
+        this.compilerSet = { };
+    }
+
+    if( !( namespace in this.compilerSet ) ||
+        this.compilerSet[ namespace ] !== compiler.constructor )
+    {
+        this.compilerSet[ namespace ] = compiler;
+    }
+};
+
+var argumentsToArray = require( "./arguments-to-array/arguments-to-array.js" );
 
 ( module || { } ).exports = CommandCompiler;
